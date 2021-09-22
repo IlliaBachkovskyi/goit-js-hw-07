c25a64da71b2ad7ec6a044368cd8851416f0a966
IlliaBachkovskyi <illiabachkovskyi@gmail.com>
2021-09-22 15:44:18 +0300

    reaches_the_goal_js-hw7_v4

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,17 @@ const ingredients = [
     'Зелень',
     'Приправы',
 ];
-  
+
+
 const ingredientsRef = document.querySelector('#ingredients');
 console.log(ingredientsRef);
-
+const allItems = [];
+ 
 ingredientsRef.style.listStyle = 'none';
-
-const elementList = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
-ingredientsRef.innerHTML = elementList;
+ 
+const addLi = ingredients.forEach(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  allItems.push(item);
+});
+ingredientsRef.append(...allItems); 
